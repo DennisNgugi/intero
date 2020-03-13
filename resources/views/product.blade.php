d03442d6779a51f07ee515d8b67eb68dc25b4f04
DennisNgugi <denongugi73@gmail.com>
2020-03-13 17:00:31 +0300

    Product detail design

---
+++ resources/views/product.blade.php
@@ -0,0 +1,258 @@
+@extends('layout')
+@section('content')
+        <!--Start breadcrumb area-->
+        <section class="breadcrumb-area style2" style="background-image: url(/images/resources/breadcrumb-bg-2.jpg);">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="inner-content-box clearfix">
+                            <div class="title-s2 text-center">
+                                <span>Single Service</span>
+                                <h1>Concept Designs</h1>
+                            </div>
+                            <div class="breadcrumb-menu float-left">
+                                <ul class="clearfix">
+                                    <li><a href="index-2.html">Home</a></li>
+                                    <li><a href="services.html">Services</a></li>
+                                    <li class="active">Single Service</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--End breadcrumb area-->
+
+        <!--Start shop area-->
+        <section id="shop-area" class="single-shop-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                        <div class="shop-content">
+                            <!--Start single shop content-->
+                            <div class="single-shop-content">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="single-product-image-holder">
+                                            <img src="images/shop/single-product.jpg" alt="Awesome Image">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="content-box">
+                                            <span class="price">$29.00</span>
+                                            <h2>Study Chair</h2>
+                                            <div class="review-box">
+                                                <ul>
+                                                    <li><i class="fa fa-star"></i></li>
+                                                    <li><i class="fa fa-star"></i></li>
+                                                    <li><i class="fa fa-star"></i></li>
+                                                    <li><i class="fa fa-star"></i></li>
+                                                    <li><i class="fa fa-star-half"></i></li>
+                                                </ul>
+                                            </div>
+                                            <div class="text">
+                                                <p>Working from home meant we couldsnack & coffee breaks change our desks or view, good, drink on the job, even spend getting roughday in pajamas consequences minus id quod maxime placeat facere possimus.</p>
+                                            </div>
+                                            <div class="location-box">
+
+                                                <span>Expected Delivery in 4-10 Days</span>
+                                            </div>
+                                            <div class="addto-cart-box">
+                                                <input class="quantity-spinner" type="text" value="2" name="quantity">
+                                                <button class="btn-one addtocart" type="submit">Add to Cart</button>
+                                            </div>
+                                            <div class="share-products-socials">
+                                                <h5>Share This Product</h5>
+                                                <ul>
+                                                    <li><a href="#"><i class="fa fa-facebook fb" aria-hidden="true"></i></a></li>
+                                                    <li><a href="#"><i class="fa fa-twitter tw" aria-hidden="true"></i></a></li>
+                                                    <li><a href="#"><i class="fa fa-pinterest pin" aria-hidden="true"></i></a></li>
+                                                    <li><a href="#"><i class="fa fa-linkedin lin" aria-hidden="true"></i></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--End single shop content-->
+
+                            <!--Start related product box-->
+                            <div class="related-product">
+                                <div class="shop-page-title">
+                                    <div class="title">Releted <span>Products</span></div>
+                                </div>
+                                <div class="row">
+                                    <!--Start single product item-->
+                                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
+                                        <div class="single-product-item text-center">
+                                            <div class="img-holder">
+                                                <img src="images/shop/4.jpg" alt="Awesome Product Image">
+                                            </div>
+                                            <div class="title-holder text-center">
+                                                <div class="static-content">
+                                                    <h3 class="title text-center"><a href="shop-single.html">Garden Chair</a></h3>
+                                                    <span>$99.00</span>
+                                                </div>
+                                                <div class="overlay-content">
+                                                    <ul class="clearfix">
+                                                        <li>
+                                                            <a href="shop-single.html"><span class="icon-cart"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Add to Cart</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><span class="icon-heart"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Add Whishlist</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><span class="icon-refresh"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Compare</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!--End single product item-->
+                                    <!--Start single product item-->
+                                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
+                                        <div class="single-product-item text-center">
+                                            <div class="img-holder">
+                                                <img src="images/shop/5.jpg" alt="Awesome Product Image">
+                                            </div>
+                                            <div class="title-holder text-center">
+                                                <div class="static-content">
+                                                    <h3 class="title text-center"><a href="shop-single.html">Office Chair</a></h3>
+                                                    <span>$74.00</span>
+                                                </div>
+                                                <div class="overlay-content">
+                                                    <ul class="clearfix">
+                                                        <li>
+                                                            <a href="shop-single.html"><span class="icon-cart"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Add to Cart</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><span class="icon-heart"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Add Whishlist</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><span class="icon-refresh"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Compare</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!--End single product item-->
+                                    <!--Start single product item-->
+                                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
+                                        <div class="single-product-item text-center">
+                                            <div class="img-holder">
+                                                <img src="images/shop/6.jpg" alt="Awesome Product Image">
+                                            </div>
+                                            <div class="title-holder text-center">
+                                                <div class="static-content">
+                                                    <h3 class="title text-center"><a href="shop-single.html">Study Lamp</a></h3>
+                                                    <span>$29.00</span>
+                                                </div>
+                                                <div class="overlay-content">
+                                                    <ul class="clearfix">
+                                                        <li>
+                                                            <a href="shop-single.html"><span class="icon-cart"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Add to Cart</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><span class="icon-heart"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Add Whishlist</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><span class="icon-refresh"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Compare</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!--End single product item-->
+                                    <!--Start single product item-->
+                                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
+                                        <div class="single-product-item text-center">
+                                            <div class="img-holder">
+                                                <img src="images/shop/8.jpg" alt="Awesome Product Image">
+                                            </div>
+                                            <div class="title-holder text-center">
+                                                <div class="static-content">
+                                                    <h3 class="title text-center"><a href="shop-single.html">Garden Chair</a></h3>
+                                                    <span>$99.00</span>
+                                                </div>
+                                                <div class="overlay-content">
+                                                    <ul class="clearfix">
+                                                        <li>
+                                                            <a href="shop-single.html"><span class="icon-cart"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Add to Cart</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><span class="icon-heart"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Add Whishlist</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><span class="icon-refresh"></span>
+                                                        <div class="toltip-content">
+                                                            <p>Compare</p>
+                                                        </div>
+                                                    </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!--End single product item-->
+                                </div>
+                            </div>
+                            <!--End related product box-->
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!--End shop area-->
+
+
+      @stop
